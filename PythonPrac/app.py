c68cf3511610983b3522966f4a2ab0dd28a2afe3
Jae Lee <jaehlee.110994@gmail.com>
2023-06-26 13:47:06 -0400

    more prac

--- PythonPrac/app.py
+++ PythonPrac/app.py
@@ -1,1 +1,7 @@
-print("hello world")
+weight = input("weight: ")
+metric = input("(K)g or (L)bs: ")
+
+if metric == "k":
+    print(float(weight) + 10)
+if metric == "l":
+    print(float(weight) + 20)
